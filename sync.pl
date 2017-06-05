d66cb9a80a07479a3b9b771fff46641fa5f4de13
c-c-l <cecilia.mcszk@gmail.com>
2017-06-05 20:27:48 +0200

    add test and connect script

--- sync.pl
+++ sync.pl
@@ -1,7 +1,7 @@
 #!/perl/bin/perl
 
 # use CGI qw(:all);
-# use DBI;
+# use DBI();
 # use verif;
 use CGI;
 use DBI;
@@ -9,8 +9,38 @@ use DBD::mysql;
 use warnings;
 use strict;
 # On ajoute la lib ldap fournie par JMA
-require "ldap_lib.pm";
+use ldap_lib;
 
+my %params;
+my $db = connect_dbi($params{'si'});
+my $ldap = connect_ldap($params{'ldap'});
+# (Ccl commentaire ->) TODO Vérifier les paramètres ldap si le nom
+# est différent le script ne s'éxécutera pas donc changer le nom dans
+# les params si nécéssaire si c'est bon effacer ce commentaire
+
+# Déclaration des variables globales (code JMA)
+my ($query, $sth, $res, $row, $user, $groupname, %expire);
+my ($lc);
+my (@adds, @mods, @dels);
+my (@SIusers, @LDAPusers);
+my (@SIgroups, @LDAPgroups);
+my ($dn, %attrib);
+
+
+# TEST (Ccl commentaire) TODO effacer ce code s'il fonctionne
+# Permet de vérifier que le lien engtre ldap et bdd est bien
+# effectué 
+
+print("Test, ce code affiche les utilisateurs de la bdd");
+$query = $cfg->val('queires', 'get_users');
+print $query."\n";
+$sth = $dbh->prepare($query);
+$res = $sth->execute;
+while($row = $sth>fetchrow_hashref) {
+	$user = $row->{identifiant};
+	push(@SIusers, $row->{identifiant});
+	printf "%S %s %s %s %s\n", $row->{identifiant}, $row->{nom}, $row->{prnom}, $row->{courriel}, $row->{id_utilisateur};
+}
 # PSEUDO-CODE
 # 
 # if connexion db not possible
