8cf787a0672f80a2bdd193faf398fc36ed825b0a
c-c-l <cecilia.mcszk@gmail.com>
2017-06-27 10:20:01 +0200

    delete data dumper for test

--- sync_test.pl
+++ sync_test.pl
@@ -9,9 +9,9 @@ use Digest::MD5 qw(md5);
 use MIME::Base64 qw(encode_base64);
 use Getopt::Long;
 use DBI();
-use Data::Dumper;
+# use Data::Dumper;
 # use strict;
-# use warnings;
+use warnings;
 
 #-----------------------------------------------------------------------
 # fonctions
